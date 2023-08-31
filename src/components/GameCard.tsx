f65a5217765dcfd85b08c6f040a6937ffd5bf587
Michael Knudson <michaelknudson@Michaels-MacBook-Pro.local>
2023-08-31 17:36:50 -0600

    added emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,9 +4,10 @@ import { Game } from '../hooks/useGames'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
 import getCroppedImageUrl from '../services/image-url'
+import Emoji from './Emoji'
 
 interface Props {
-    game: Game
+    game: Game;
 }
 
 const GameCard = ({ game }: Props) => {
@@ -18,7 +19,7 @@ const GameCard = ({ game }: Props) => {
             <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
             <CriticScore score={game.metacritic} />
             </HStack>
-            <Heading fontSize={'2xl'}>{game.name}</Heading>
+            <Heading fontSize={'2xl'}>{game.name}<Emoji rating={game.rating_top} /></Heading>
         </CardBody>
 
     </Card>
